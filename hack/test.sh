44625e5199dabaf1f90c92b78ff1dd524649136d
Dmitriy Kalinin <dkalinin@vmware.com>
2020-12-13 15:50:22 -0500

    fix line parsing of README.md for tests

--- hack/test.sh
+++ hack/test.sh
@@ -6,10 +6,12 @@ set -x -u
 
 read -d '' ytt_test << EOF
 ytt_lines = File.readlines("./README.md")
-ytt_lines = ytt_lines.select { |line| line.start_with?("ytt") and line.include?("-f") }
+ytt_lines = ytt_lines.select { |line| line.include?("ytt") and line.include?("-f") }
+ytt_lines = ytt_lines.map { |line| line.split("<(")[1] }
 ytt_lines = ytt_lines.map { |line| line.split("|")[0] }
+ytt_lines = ytt_lines.map { |line| line.split(")")[0] }
 raise "Expected at least one ytt command" if ytt_lines.empty?
-ytt_lines.each { |line| system(line + ">/dev/null && 2>&1") or raise "Failed on '#{line}'" }
+ytt_lines.each { |line| system(line + ">/dev/null 2>&1") or raise "Failed on '#{line}'" }
 puts "YTT CHECKS SUCCESS"
 EOF
 
